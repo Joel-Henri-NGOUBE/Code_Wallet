80fac76220835c21f582688297c408cd6593cd5d
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-01-06 00:39:07 +0100

    Tags page finished

--- code_wallet/src/Pages/Tags/Tags.tsx
+++ code_wallet/src/Pages/Tags/Tags.tsx
@@ -37,6 +37,30 @@ export default function Tags(){
                 </div>
             </div>))}
         </div>
+
+        <div className="modal-container">
+            {/* <div className="modal-new">
+                <h2>New tag</h2>
+                <p className="close">X</p>
+                <div className="form">
+                    <label htmlFor="tag-name">TagName</label>
+                    <input type="text" name="tag-name" id="tag-name" placeholder="TagName..." />
+                </div>
+                <button>Save</button>
+            </div> */}
+            <div className="modal-edit">
+            <h2>Edit tag</h2>
+                <p className="close">X</p>
+                <div className="form">
+                    <label htmlFor="tag-name2">TagName</label>
+                    <input type="text" name="tag-name2" id="tag-name2" placeholder="TagName..." />
+                </div>
+                <div className="buttons">
+                    <button id="delete">Delete</button>
+                    <button id="save">Save</button>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
