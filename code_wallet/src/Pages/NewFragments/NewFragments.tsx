86263405e1e171b0a337417d22194a5d304cded8
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-01-06 03:07:40 +0100

    Fragments page started

--- code_wallet/src/Pages/NewFragments/NewFragments.tsx
+++ code_wallet/src/Pages/NewFragments/NewFragments.tsx
@@ -1,7 +1,39 @@
+import Header from "../../Components/Header/Header"
+import "./newfragments.css"
+import Remove from "../../assets/remove.svg"
+
 export default function NewFragments(){
+   const tags = ["Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag", "Tag"]
+
     return (
-    <>
-        <p>Bonjour les nouveaux fragments</p>
-    </>
+    <div className="newfragments">
+        <Header />
+        <div className="title">
+            <h1>Fragments</h1>
+        </div>
+        <div className="form">
+            <div className="fragment-title">
+                <label htmlFor="title"><h2>Title</h2></label>
+                <input type="text" placeholder="Title..." name="title" id="title"/>
+            </div>
+            <div className="tags-label">
+                <span id="tag"><h2>Tags</h2></span>
+                <div className="tags">
+                    <div className="tags-list">
+                    {tags.map((tag, index) => <span key={index} className="tag">{tag}<img src={Remove} alt="remove" /></span>)}
+                    </div>
+                    <button>+ Add tag</button>
+                </div>
+            </div>
+            <div className="code">
+                <label htmlFor="code"><h2>Code</h2></label>
+                <textarea name="code" id="code" placeholder="Write your code here..."></textarea>
+            </div>
+        </div>
+        <div className="buttons">
+            <button id="delete">Delete</button>
+            <button id="save">Save</button>
+        </div>
+    </div>
     )
 }
